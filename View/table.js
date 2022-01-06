cac0d9f2fc3db6dc35243a51868ce06c7d82d58d
ggenelhoud <genelha17@gmail.com>
2022-01-06 22:45:25 +0000

    input validation check

--- View/table.js
+++ View/table.js
@@ -14,35 +14,47 @@ function draw_table() {
     $.getHTMLuncached("/get/html");
 };
 
-function add_movie(){
+function add_movie() {
     $("body").on("click", ".insertRowBtn", function () {
-        
+
     });
 }
 
 function delete_movie() {
     $("body").on("click", ".deleteBtn", function () {
-        if(confirm("Are you sure you want to remove this?"))
-        $(this).parents('tr').remove();
+        if (confirm("Are you sure you want to remove this?"))
+            $(this).parents('tr').remove();
     });
 };
 
-function insertNewMovie(){
-    event.preventDefault();
+function insertNewMovie() {
+    
+    if (titleName.value == "" ||
+        genreName.value == "" ||
+        directorName.value == "" ||   
+        studioName.value == "" ||
+        yDate.value == "" )
+    {
+        alert("You MUST fill in ALL the fields!!!");
+        document.formname.FirstName.focus();
+        return false;
+    }
+    // event.preventDefault();
     var table = document.getElementById("moviesTable").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     var cell1 = newRow.insertCell(0);
-        cell1.innerHTML = titleName.value;
+    cell1.innerHTML = titleName.value;
     var cell2 = newRow.insertCell(1);
-        cell2.innerHTML = genreName.value;
+    cell2.innerHTML = genreName.value;
     var cell3 = newRow.insertCell(2);
-        cell3.innerHTML = directorName.value;
+    cell3.innerHTML = directorName.value;
     var cell4 = newRow.insertCell(3);
-        cell4.innerHTML = studioName.value;
+    cell4.innerHTML = studioName.value;
     var cell5 = newRow.insertCell(4);
-        cell5.innerHTML = yDate.value;
+    cell5.innerHTML = yDate.value;
     var cell6 = newRow.insertCell(5);
-        cell6.innerHTML = `<button class="deleteBtn">Delete</button>`
+    cell6.innerHTML = `<button class="deleteBtn">Delete</button>`
+
 }
 
 $(document).ready(function () {
